3ea88ed147b67c57bcad62390e2d7630e17b0fb1
myang <kmh4503@gmail.com>
2021-02-09 21:00:35 +0900

    Test용 커밋

---
+++ temp.py
@@ -0,0 +1,5 @@
+﻿[KyoboBook.Ebook.Platform][17:22:33.75][Info] [Config] 설정 파일 없음, 기본값 사용
+[KyoboBook.Ebook.Platform][17:22:34.06][Info] 마지막 로그 아웃된 상태로 시작 됨.
+[KyoboBook.Ebook.Platform][17:22:37.50][Info] 마지막 로그 아웃된 상태로 시작 됨.
+[KyoboBook.Ebook.Platform][20:52:35.77][Info] 마지막 로그 아웃된 상태로 시작 됨.
+[KyoboBook.Ebook.Platform][20:53:03.98][Info] 마지막 로그 아웃된 상태로 시작 됨.
